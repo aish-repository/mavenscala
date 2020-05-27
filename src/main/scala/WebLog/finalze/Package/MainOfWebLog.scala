9f2de91b8d408c39f89422ea6ec6a1c31ec895ce
aish-repository <aishwaryabala@gmail.com>
2020-05-28 03:48:45 +0530

    inital commit

--- src/main/scala/WebLog/finalze/Package/MainOfWebLog.scala
+++ src/main/scala/WebLog/finalze/Package/MainOfWebLog.scala
@@ -93,7 +93,7 @@ object MainOfWebLog {
             //email domain masking using regex
             //removed special characters from phone number
             //age is encrypted with sha2 256 bits
-            //position masking for the ip
+            //position masking for the ip address
             val finaldf = spark.sql(
               """
                     select concat(usd.username,day,month,yr,hr,mt,sec) as custid
